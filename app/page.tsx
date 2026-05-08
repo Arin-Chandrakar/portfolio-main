3495edf96ea4802829e7e16c1126aaa8858efdf9
Arin-Chandrakar <arin23official@gmail.com>
2026-05-08 11:58:52 +0530

    twitter like layout 1/2

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-            <div className="relative">
-                 <img src="/Hanuman.png" className="" alt="" />
+        <div className="relative w-full">
+                 <img src="/Hanuman.png" className="w-full" alt="" />
                  <div className="absolute top-0 left-0 h-5 w-full bg-linear-to-b from-black to-transparent "/>
-            </div>
-            <div className=" md:top-100 md:left-14 absolute sm:left-10 sm:top-10" >
-                 <div className="aspect-square rounded-full border w-55 border-white/50 border-2 p-0.25">
+            <div className="absolute md:left-14 md:top-75 left-7 top-32" >
+                 <div className="aspect-square rounded-full md:w-55 w-24 border-white/50 border-2 p-px">
                     <img src="/favicon.png" className="" alt="" />
                  </div>
                  
             </div>
+        </div>
+            
 
 
 
